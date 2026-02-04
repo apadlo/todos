af606be400d50e7902578462fdc9465b1c99e44e
apadlo <apadlo@hotmail.com>
2026-02-04 09:57:52 +0100

    Configured DEFAULT_AUTO_FIELD

--- todo/apps.py
+++ todo/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class TodoConfig(AppConfig):
     name = 'todo'
+    default_auto_field = 'django.db.models.BigAutoField'
--- todowoo/settings.py
+++ todowoo/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_URL = '/login'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
